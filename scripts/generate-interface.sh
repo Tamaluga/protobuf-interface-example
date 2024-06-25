93030752ddf4154a8f0d42f903c526dfe95669a0
Frutiger Daniel <daniel.frutiger@betterask.erni>
2024-06-25 17:11:01 +0200

    Correct script

--- scripts/generate-interface.sh
+++ scripts/generate-interface.sh
@@ -3,6 +3,14 @@
 # Enable strict error handling: exit on error (-e), treat unset variables as an error (-u), and fail if any command in a pipeline fails (-o pipefail)
 set -euo pipefail 
 
+# switch to project top folder
+SCRIPT_PATH=$(dirname $(readlink -f "$0"))
+pushd "${SCRIPT_PATH}/.." > /dev/null
+
+############################################################
+# Main program                                             #
+############################################################
+
 # Constants
 readonly INTERFACE_FOLDER="interface"
 readonly INTERFACE_FOLDER_C="${INTERFACE_FOLDER}/c"
@@ -13,3 +21,6 @@ mkdir -p ${INTERFACE_FOLDER_C} ${INTERFACE_FOLDER_C_SHARP}
 
 # Generate proto interface
 protoc --proto_path=${INTERFACE_FOLDER} --cpp_out=${INTERFACE_FOLDER_C} --csharp_out=${INTERFACE_FOLDER_C_SHARP} Robot.proto 
+
+# Switch to the origin directory
+popd > /dev/null
